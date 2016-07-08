6c7fe79198015d0299d6a47beda27a36cbf2bef1
Martijn <tijntje_7@msn.com>
2016-07-08 14:48:30 +0200

    remove log statements and improve comments slightly

--- src/background.js
+++ src/background.js
@@ -20,16 +20,12 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     var url = tab.url;
 
-    console.log("changed tab!");
-
     if (url.indexOf("martijn") !== -1) {
-      console.log("setting icon to check");
       chrome.browserAction.setIcon({
         path: "resources/check-128.png",
         tabId: tab.id
       });
     } else {
-      console.log("setting icon to smg");
       chrome.browserAction.setIcon({
         path: "resources/icon-60.png",
         tabId: tab.id
